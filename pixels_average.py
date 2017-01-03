60aaf4a96495cf6609d92423b3f0dd5f2f594e9a
wiktorwalentynowiczit <wiktorwalentynowiczit@gmail.com>
2017-01-03 20:19:37 +0100

    refactor of input files

--- pixels_average.py
+++ pixels_average.py
@@ -1,2 +1,12 @@
+import glob
+
 from scipy import misc
 
+images = glob.glob('input/*.jpg')
+print(images)
+
+np_images = []
+for path in images:
+    np_images.append(misc.imread(path))
+
+np_images[0].shape(), np_images[0].dtype()

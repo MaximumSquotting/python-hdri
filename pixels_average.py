4f461820041ee8a5e4a16edc27ee3f6f19b7efd6
wiktorwalentynowiczit <wiktorwalentynowiczit@gmail.com>
2017-01-03 20:38:11 +0100

    full implementation of color version of average pixels

--- pixels_average.py
+++ pixels_average.py
@@ -1,12 +1,20 @@
 import glob
+import numpy as np
 
 from scipy import misc
 
 images = glob.glob('input/*.jpg')
-print(images)
 
 np_images = []
 for path in images:
     np_images.append(misc.imread(path))
 
-np_images[0].shape(), np_images[0].dtype()
+hdr_image = np.zeros(np_images[0].shape, np_images[0].dtype)
+
+for npi in np_images:
+    hdr_image = np.add(hdr_image, npi)
+
+hdr_image = hdr_image // len(np_images)
+
+misc.imsave('output/output.jpg', hdr_image)
+misc.imsave('output/test.jpg', np_images[0])

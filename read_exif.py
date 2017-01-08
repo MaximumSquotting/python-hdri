de408d2a74b939c5f20d1ec50c94e2c87fd0e40b
Michał Zając <michal.zajac@gmail.com>
2017-01-08 18:07:26 +0100

    +read_exif.py

---
+++ read_exif.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import pyexiv2
+import ipdb
+
+def get_exposure(photo):
+    metadata = pyexiv2.ImageMetadata(photo)
+    metadata.read()
+    try:
+        exposure_fraction = metadata['Exif.Photo.ExposureTime'].value
+        return round(float(exposure_fraction), 2)
+    except KeyError:
+        print("%{0} lacks ExposureTime EXIF tag. Skipping.".format(photo))
+        pass
+
+parser = argparse.ArgumentParser()
+parser.add_argument("images", nargs="+", help="images to get the values from")
+args = parser.parse_args()
+
+for filename in args.images:
+    exposure = get_exposure(filename)
+    filename_split = os.path.splitext(filename)
+    new_filename = "t{0}{1}".format(exposure, filename_split[1].lower())
+    print("Renaming {0} to {1}".format(filename, new_filename))
+    os.rename(filename, new_filename)

f15a35c85f5aa96287a289453c6a4f060397a864
wiktorwalentynowiczit <wiktorwalentynowiczit@gmail.com>
2017-01-23 18:15:02 +0100

    last changes

--- matplot_hsv_average.py
+++ matplot_hsv_average.py
@@ -14,8 +14,7 @@ for cri in color_images:
 
 color_image /= len(color_images)
 color_image = mc.hsv_to_rgb(color_image)
-print(color_image)
+
 color_image *= 255
-print(color_image)
 
 mpimg.imsave('output/output_color_hsv.jpg', color_image.astype('uint8'))
--- matplot_rgb_average.py
+++ matplot_rgb_average.py
@@ -2,15 +2,27 @@ import matplotlib.image as mpimg
 import numpy as np
 import glob
 
+def rgb2gray(rgb):
+    matrix = np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+    return np.dstack((matrix, matrix, matrix))
+
 images = glob.glob('input/*.jpg')
 color_images = []
 for path in images:
     color_images.append(mpimg.imread(path))
 
+gray_images = list(map(rgb2gray, color_images))
+gray_image = np.zeros(gray_images[0].shape, gray_images[0].dtype)
+
 color_image = np.zeros(color_images[0].shape)
 for cri in color_images:
     color_image = np.add(color_image, cri)
 
+for gri in gray_images:
+    gray_image = np.add(gray_image, gri)
+
 color_image /= len(color_images)
-print(color_image)
+gray_image /= len(gray_images)
+
 mpimg.imsave('output/output_color_rgb.jpg', color_image.astype('uint8'))
+mpimg.imsave('output/output_gray_rgb.jpg', gray_image.astype('uint8'))
